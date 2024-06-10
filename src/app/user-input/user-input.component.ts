c5641a4c97485d15c274ea233840c1d136650d9f
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2024-06-10 14:09:18 +0530

    forms: added user-input, form bindings, data logic

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { type InvestmentInput } from '../investment-input.model';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
-  styleUrl: './user-input.component.css'
+  styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
+  @Output() calculate = new EventEmitter<InvestmentInput>();
 
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedResult = '5';
+  enteredDuration = '10';
+
+  onSubmit() {
+    this.calculate.emit({
+      initialInvestment: Number(this.enteredInitialInvestment),
+      duration: Number(this.enteredDuration),
+      expectedReturn: Number(this.enteredExpectedResult),
+      annualInvestment: Number(this.enteredAnnualInvestment),
+    });
+  }
 }
