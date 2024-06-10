2444958b42e379e4a85f1ac67ea0da68b47180e5
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2024-06-10 12:42:12 +0530

    chore: register user-input component to the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component';
+import { UserInputComponent } from './user-input/user-input.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, UserInputComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
