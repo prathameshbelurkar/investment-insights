b03c80c0e475c08e28ff4318284c877f97b5945d
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2024-06-10 14:09:55 +0530

    feat: logic fn addition, bind input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,43 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
 
+import type {
+  InvestmentInput,
+  InvestmentOutput,
+} from './investment-input.model';
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [HeaderComponent, UserInputComponent],
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  annualData?: InvestmentOutput[];
+
+  onCalculateInvestmentResults(data: InvestmentInput) {
+    const { annualInvestment, duration, expectedReturn, initialInvestment } =
+      data;
+
+    const annualData = [];
+    let investmentValue = initialInvestment;
+
+    for (let i = 0; i < duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest =
+        investmentValue - annualInvestment * year - initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      });
+    }
+
+    this.annualData = annualData;
+  }
+}
